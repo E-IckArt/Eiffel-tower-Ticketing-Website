df7dd16e587f5e883e1747c96408e800d6aea9f2
Emilie Ickart <emi.ickart@gmail.com>
2022-08-31 16:16:14 +0200

    Ft - Update JavaScript functions

--- index.js
+++ index.js
@@ -1,67 +1,70 @@
-function displayAdultPrice() {
-    let adults = document.getElementById('adults').value;
-    let children = document.getElementById('children').value;
-    let displayTotalAdultsTickets =
-        document.getElementById('totalAdultsTickets');
-    let displayTotalPrice = document.getElementById('displayTotalPrice');
-    let price = calc(adults, children);
-    if (adults < 0) {
-        this.value = 0;
-        return this.value;
-    }
+let getNumberOfAdults = document.getElementById('adults');
+let getNumberOfChildren = document.getElementById('children');
 
-    if (price > 0) {
-        displayTotalAdultsTickets.textContent =
-            ' ' + this.value + ' ' + 'x 12 €';
-        displayTotalPrice.textContent = price + ' €';
-    }
-}
+let displayTotalAdultsTickets = document.getElementById('totalAdultsTickets');
+let displayTotalChildrenTickets = document.getElementById(
+    'totalChildrenTickets'
+);
+let displayTotalPrice = document.getElementById('displayTotalPrice');
 
-function displayChildrenPrice() {
-    let adults = document.getElementById('adults').value;
-    let children = document.getElementById('children').value;
-    let displayTotalChildrenTickets = document.getElementById(
-        'totalChildrenTickets'
-    );
-    let displayTotalPrice = document.getElementById('displayTotalPrice');
-    let price = calc(adults, children);
-    if (children < 0) {
-        this.value = 0;
-        return this.value;
-    }
-    displayTotalChildrenTickets.textContent = ' ' + this.value + ' ' + 'x 5 €';
-    displayTotalPrice.textContent = price + ' €';
-}
+let btn = document.getElementById('button');
 
-function calc(adults, children) {
-    // Récupére le noeud du DOM où le message doit apparaître
-    let help = document.getElementById('help');
-    // Déclaration d'une variable pour le message d'alerte
-    let warningMessage;
+function displayWarningMessage() {
+    // Récupère la valeur entrée par l'utilisateur
+    let adults = getNumberOfAdults.value;
+    let children = getNumberOfChildren.value;
+    let help = document.getElementById('help'); // Récupére le noeud du DOM où le message doit apparaître
+    let warningMessage; // Déclaration d'une variable pour le message d'alerte
 
     if (adults < 0) {
         adults = 0;
-        warningMessage = 'Please enter a positive number';
-    }
-
-    if (children < 0) {
+        warningMessage = 'Please enter a positive number for adult ticket';
+    } else if (children < 0) {
         children = 0;
-        warningMessage = 'Please enter a positive number';
-    }
-
-    if (adults == 0 && children > 0) {
+        warningMessage = 'Please enter a positive number for children ticket';
+    } else if (adults == 0 && children > 0) {
         warningMessage =
             'For safety reasons children must be accompanied by at least one adult.';
-    } else {
-        help.textContent = ' ';
     }
+
     // Ajout de style au msg
     help.style.color = '#fdff75';
     help.style.textAlign = 'center';
-    // Ajoute le message dans l'espace réservé
-    help.textContent = warningMessage;
+
+    help.textContent = warningMessage; // Ajoute le message dans l'espace réservé
+}
+
+function displayAdultPrice() {
+    displayWarningMessage();
+    // let price = calc(adults, children);
+
+    // Eviter l'affichage de valeur négative
+    if (this.value < 0) {
+        this.value = 0;
+        return this.value;
+    }
+    displayTotalAdultsTickets.textContent = ` ${this.value * 12} €`;
+    // displayTotalPrice.textContent = price + ' €';
+}
+
+function displayChildrenPrice() {
+    displayWarningMessage();
+    // let price = calc(adults, children);
+
+    // Eviter l'affichage de valeur négative
+    if (this.value < 0) {
+        this.value = 0;
+        return this.value;
+    }
+    displayTotalChildrenTickets.textContent = ` ${this.value * 5} €`;
+    // displayTotalPrice.textContent = price + ' €';
+}
+
+function calc(adults, children) {
     // Retourne le calcul du prix total des billets
-    return adults * 12 + children * 5;
+    const price = adults * 12 + children * 5;
+    displayTotalPrice.textContent = price + ' €';
+    return price;
 }
 
 function textToBold() {
@@ -72,14 +75,13 @@ function textToBold() {
 }
 
 window.onload = () => {
-    let getNumberOfAdults = document.getElementById('adults');
-    let getNumberOfChildren = document.getElementById('children');
-    let btn = document.getElementById('button');
-
     getNumberOfAdults.addEventListener('change', displayAdultPrice);
     getNumberOfChildren.addEventListener('change', displayChildrenPrice);
 
-    btn.addEventListener('click', function () {
+    btn.addEventListener('click', () => {
+        let adults = getNumberOfAdults.value;
+        let children = getNumberOfChildren.value;
+        calc(adults, children);
         textToBold();
         getNumberOfAdults.removeEventListener('change', displayAdultPrice);
         getNumberOfChildren.removeEventListener('change', displayChildrenPrice);
